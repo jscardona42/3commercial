70a5f3d3098b2c13d6d7f756f442df41f6d584e2
jscardona42 <35873185+jscardona42@users.noreply.github.com>
2021-03-19 08:18:37 -0500

    Llamamos microservicio para guardar métodos en la base de datos.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,11 @@ import { PrismaService } from './prisma.service';
 import { Product } from './products/product.entity';
 import { ProductResolver } from './products/product.resolver';
 import { ProductService } from './products/product.service';
+const fetch = require('node-fetch');
+
+const apiUrl = "http://localhost:3003";
+const headers = { 'Content-Type': 'application/json' }
+const MyProviders = [PrismaService, ProductResolver, ProductService]
 
 
 @Module({
@@ -17,6 +22,26 @@ import { ProductService } from './products/product.service';
     })
   ],
   controllers: [],
-  providers: [PrismaService, ProductResolver, ProductService],
+  providers: MyProviders,
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    setTimeout(() => {
+      this.saveProviders();
+    }, 300);
+  }
+
+  public saveProviders() {
+    for (const clsname of MyProviders) {
+      var TMPmethods = Object.getOwnPropertyNames(clsname.prototype).filter(
+        item => item !== 'constructor'
+      );
+      fetch(`${apiUrl}/admin`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({ TMPmethods, clsname: clsname.name })
+      }).then(response => response.json())
+        .then(response => console.log(response))
+    }
+  }
+}
